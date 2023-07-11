0c84e377decdd8fc1335b5a69bbaf45306ba2cdf
mukhametzyanovra <rikitavi777@yandex.ru>
2023-07-11 10:11:55 +0300

    Реализован read из CRUD

---
+++ src/main/java/ru/mukhametzyanov/springcourse/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package ru.mukhametzyanov.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.mukhametzyanov.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Ruslan"));
+        people.add(new Person(++PEOPLE_COUNT, "Dzhamil"));
+        people.add(new Person(++PEOPLE_COUNT, "Iskander"));
+        people.add(new Person(++PEOPLE_COUNT, "Iskander2"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny().orElse(null);
+    }
+
+}
